113e81008fd3c114657f832b8011df256c1dd355
MKowalski234 <58140230+MKowalski234@users.noreply.github.com>
2023-03-06 10:18:22 +0100

    Add files via upload

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,14 +1,36 @@
-from django.http import HttpResponse
+"""
+This module provides utility functions for rendering templates and returning HTTP responses.
+"""
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def hello_world(request):
+
+
+def hello_world() -> HttpResponse:
+    """
+    Returns a HttpResponse object that displays a "Hello, World!" message.
+
+    Returns:
+        HttpResponse: A HTTP response object with "Hello, World!" message.
+    """
     return HttpResponse("Hello, World!")
 
 
-def greet(request):
+
+
+def greet(request) -> HttpResponse:
+    """
+    Greet the user with their name if request is received, else render the 'hello.html' template.
+
+    Args:
+        request (HttpRequest): The request object passed by Django.
+
+    Returns:
+        HttpResponse: A HTTP response object with a greeting message or a rendered template.
+    """
     if request.method == 'POST':
         name = request.POST['name']
         return HttpResponse(f'Hello {name}!')
-    else:
-        return render(request, 'templates/hello.html')
+
+    return render(request, 'templates/hello.html')
